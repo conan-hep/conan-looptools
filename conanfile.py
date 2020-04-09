aaad20d98cfcab4701fcc3ea7a2aa82ef53606ce
Alexander Voigt <ahgvoigt@gmail.com>
2020-04-09 15:45:29 +0200

    enforce linking with libquadmath
    which is required by LoopTools

--- conanfile.py
+++ conanfile.py
@@ -86,3 +86,5 @@ class LoopToolsConan(ConanFile):
 
     def package_info(self):
         self.cpp_info.libs = ["ooptools"]
+        self.cpp_info.exelinkflags.append('-lquadmath')
+        self.cpp_info.sharedlinkflags = self.cpp_info.exelinkflags
