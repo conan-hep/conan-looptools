3eaf7787760d06ba047c2d30f0f78b4e594eb54a
Alexander Voigt <ahgvoigt@gmail.com>
2020-05-08 14:41:24 +0200

    correcting setting of packager

--- build.py
+++ build.py
@@ -2,6 +2,6 @@ from cpt.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(settings={"arch": "x86_64"})
-    builder.add_common_builds()
+    builder = ConanMultiPackager()
+    builder.add(settings={"arch": "x86_64"})
     builder.run()
