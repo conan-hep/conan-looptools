eb77ef1f161fd7eeda8739e821facdef2279adcd
Alexander Voigt <ahgvoigt@gmail.com>
2020-05-08 14:38:49 +0200

    build only for 64 bit architectures

--- build.py
+++ build.py
@@ -2,6 +2,6 @@ from cpt.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager()
+    builder = ConanMultiPackager(settings={"arch": "x86_64"})
     builder.add_common_builds()
     builder.run()
