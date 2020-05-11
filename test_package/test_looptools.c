2e7ddad24a4ac48edfb2cc85239229d8dd935672
Alexander Voigt <ahgvoigt@gmail.com>
2020-05-11 17:55:33 +0200

    try to fix Fortran runtime error

--- test_package/test_looptools.c
+++ test_package/test_looptools.c
@@ -9,11 +9,11 @@ int main()
    ComplexType c0  = C0(      0., 1., 3., 10., 20., 30.);
    ComplexType c0i = C0i(cc0, 0., 1., 3., 10., 20., 30.);
 
+   ltexi();
+
    printf("%e + %e*I\n", Re(b0 ), Im(b0 ));
    printf("%e + %e*I\n", Re(c0 ), Im(c0 ));
    printf("%e + %e*I\n", Re(c0i), Im(c0i));
-
-   ltexi();
 
    return 0;
 }
