2e258ab28911278690097bda6cce1fd50d2a7cd4
Alexander Voigt <ahgvoigt@gmail.com>
2020-05-11 11:35:02 +0200

    call C0i function

--- test_package/test_looptools.c
+++ test_package/test_looptools.c
@@ -5,11 +5,13 @@ int main()
 {
    ltini();
 
-   ComplexType b0 = B0(1000., 50., 80.);
-   ComplexType c0 = C0(0., 1., 3., 10., 20., 30.);
+   ComplexType b0  = B0(1000., 50., 80.);
+   ComplexType c0  = C0(      0., 1., 3., 10., 20., 30.);
+   ComplexType c0i = C0i(cc0, 0., 1., 3., 10., 20., 30.);
 
-   printf("%e + %e*I\n", Re(b0), Im(b0));
-   printf("%e + %e*I\n", Re(c0), Im(c0));
+   printf("%e + %e*I\n", Re(b0 ), Im(b0 ));
+   printf("%e + %e*I\n", Re(c0 ), Im(c0 ));
+   printf("%e + %e*I\n", Re(c0i), Im(c0i));
 
    ltexi();
 
